b8c56c19ea49c4ac65694b8294897518e6641c8f
Brian Ho <brian@vandrico.com>
2017-04-30 07:38:41 -0700

    further testing the promise pattern

--- index.js
+++ index.js
@@ -2,28 +2,21 @@ const Promise = require('bluebird');
 const Picatic = require('./lib/picatic');
 let api_options = require('./access_key.js')
 
-api_options.api_version = 'v1';
+api_options.api_version = 'v2';
 
 let client = new Picatic(api_options);
 let params = {};
 
-client.get('user/me', params, (err, data, res) => {
-	console.log(err,data,res);
-});
+// client.get('user/me', params, (err, data, res) => {
+// 	 console.log(data);
+// });
 
-/*
- request({
- uri:'users/me',
- headers: {
- "X-Picatic-Access-Key": "c90ce778d4d907d08b7b14654c9ce6f599243a16",
- 'content-type': 'application/json'
- }
- }, (err, res, body) => {
+// client.get('user/577072', params)
+// 	.then((data) =>{
+// 		console.log(data);
+// 	})
 
- if (err) console.log(err);
- else {
- console.log(JSON.parse(body));
- }
- });
-
- */
+client.get('event', params)
+	.then((data) =>{
+		console.log(data);
+	})
